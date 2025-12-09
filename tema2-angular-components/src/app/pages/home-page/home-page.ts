da97de654ddf3eb93ad350bbca8eee323030bc10
BMWGengiuRobert <robert.gengiu@bmwtechworks.ro>
2025-12-09 17:11:55 +0200

    projects card created in home page

--- tema2-angular-components/src/app/pages/home-page/home-page.ts
+++ tema2-angular-components/src/app/pages/home-page/home-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CustomDatePipe } from '../../pipes/custom-date.pipe';
 import { MyTasks } from './my-tasks/my-tasks';
 import { MyGoals } from './my-goals/my-goals';
+import { ProjectsCard } from './projects-card/projects-card';
 
 @Component({
   selector: 'app-home-page',
-  imports: [CustomDatePipe, MyTasks, MyGoals],
+  imports: [CustomDatePipe, MyTasks, MyGoals, ProjectsCard],
   templateUrl: './home-page.html',
   styleUrl: './home-page.sass',
 })
