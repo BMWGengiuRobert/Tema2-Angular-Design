6718c8749a62fbceab94e59fab44a0975a4316a7
BMWGengiuRobert <robert.gengiu@bmwtechworks.ro>
2025-12-09 16:10:00 +0200

    my goals card added to home page

--- tema2-angular-components/src/app/pages/home-page/home-page.ts
+++ tema2-angular-components/src/app/pages/home-page/home-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CustomDatePipe } from '../../pipes/custom-date.pipe';
 import { MyTasks } from './my-tasks/my-tasks';
+import { MyGoals } from './my-goals/my-goals';
 
 @Component({
   selector: 'app-home-page',
-  imports: [CustomDatePipe, MyTasks],
+  imports: [CustomDatePipe, MyTasks, MyGoals],
   templateUrl: './home-page.html',
   styleUrl: './home-page.sass',
 })
